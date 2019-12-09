2801ccc55b22022b8be80b68672239ad63cc1a8a
pthv3004 <58245176+pthv3004@users.noreply.github.com>
2019-12-09 16:30:54 +0700

    ProfileScreen

---
+++ EPayroll/EPayroll/Models/Profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPayroll.Models
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int Age { get; set; }
+        public bool Gender { get; set; }
+        public string IdentifyNumber { get; set; }
+        public string Mode { get; set; }
+        public string Level { get; set; }
+
+    }
+}
--- EPayroll/EPayroll/ViewModels/ProfileViewModel.cs
+++ EPayroll/EPayroll/ViewModels/ProfileViewModel.cs
@@ -5,6 +5,7 @@ using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace EPayroll.ViewModels
@@ -16,24 +17,35 @@ namespace EPayroll.ViewModels
         public ProfileViewModel(IEmployeeService employeeService, INavigationService navigationService) : base(navigationService)
         {
             _employeeService = employeeService;
-            var details = employeeService.GetDetail(new Guid("515f1407-f98f-4f15-8459-08d778b630cb")).Result;
-            Name = details.Name;
-            Position = details.Position;
-            Age = details.Age;
-            Gender = details.Gender;
-            IdentifyNumber = details.IdentifyNumber;
-            Mode = details.SalaryMode;
-            Level = details.SalaryLevel;
+            Task.Run(async () =>
+            {
+                var details = await employeeService.GetDetail(new Guid("515f1407-f98f-4f15-8459-08d778b630cb"));
+                Name = details.Name;
+                Position = details.Position.Name;
+                Age = details.Age;
+                Gender = details.Gender;
+                IdentifyNumber = details.IdentifyNumber;
+                Mode = details.SalaryMode.Mode;
+                Level = details.SalaryLevel.Level;
+            });
         }
 
+        private string _name;
+        private string _position;
+        private int _age;
+        private bool _gender;
+        private string _identifyNumber;
+        private string _mode;
+        private string _level;
+
         #region 
-        public string Name { get; set; }
-        public PositionServiceViewModel Position { get; set; }
-        public int Age { get; set; }
-        public bool Gender { get; set; }
-        public string IdentifyNumber { get; set; }
-        public SalaryModeServiceViewModel Mode { get; set; }
-        public SalaryLevelServiceViewModel Level { get; set; }
+        public string Name { get { return _name; } set { SetValue(ref _name, value); } }
+        public string Position { get { return _position; } set { SetValue(ref _position, value); } }
+        public int Age { get { return _age; } set { SetValue(ref _age, value); } }
+        public bool Gender { get { return _gender; } set { SetValue(ref _gender, value); } }
+        public string IdentifyNumber { get { return _identifyNumber; } set { SetValue(ref _identifyNumber,value); } }
+        public string Mode { get { return _mode; } set { SetValue(ref _mode,value); } }
+        public string Level { get { return _level; } set { SetValue(ref _level,value); } }
         #endregion
         public ImageSource ImageSource { get { return ImageSource.FromResource("EPayroll.Images.hana.jpg"); } }
     }
